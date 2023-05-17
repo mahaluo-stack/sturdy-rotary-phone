abe25ad1a91f6485bf995b522e74ec35ecad1df2
mahaluo <max.haggqvist@gmail.com>
2023-05-17 10:21:40 +0200

    cleaned up routes

--- src/app/core/routes/tablet.routes.ts
+++ src/app/core/routes/tablet.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import * as ResponsivePages from '../components/pages/responsive';
+
 import { responsiveRoutes } from "./responsive.routes";
 
 const routes: Routes = [
